f23102de29f7d48ea50dae2cbb815b080fa13b38
DerekGn <derekgn@users.noreply.github.com>
2025-07-31 07:12:19 +0100

    Updated suppression file

--- src/Shaos.Sdk.UnitTests/GlobalSuppressions.cs
+++ src/Shaos.Sdk.UnitTests/GlobalSuppressions.cs
@@ -6,5 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "<Pending>", Scope = "namespace", Target = "~N:Shaos.Sdk.UnitTests")]
-[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "<Pending>", Scope = "namespace", Target = "~N:Shaos.Sdk.UnitTests.Collections.Generic")]
-[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "<Pending>", Scope = "namespace", Target = "~N:Shaos.Sdk.UnitTests.Devices.Parameters")]
+[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "<Pending>", Scope = "namespace", Target = "~N:Shaos.Sdk.UnitTests.Devices")]
